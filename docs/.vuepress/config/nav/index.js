3ecb54568aff9efda95cd0c919c1f0b0454c6c7e
aqwy1996822 <aqwy1996822@163.com>
2021-08-09 15:26:38 +0800

    update docs

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -1,7 +1,7 @@
 module.exports = [
   { text: '主页', link: '/', icon: 'reco-home' },
   { text: '时间轴', link: '/timeline/', icon: 'reco-date' },
-  { text: '周志', link: '/weekly_summary/', icon: 'reco-document' },
+  { text: '周志', link: '/categories/学习周志/', icon: 'reco-document' },
   { text: '友链',
     icon: 'reco-message',
     items: [
